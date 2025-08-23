ddf05ce0bf1b00e86e83b53d65a28a49e26374c3
AdityaSahu15 <itsadityasahu1509@gmail.com>
2025-08-23 18:48:57 +0530

    basic setup of the project and home page's components designed

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -4,12 +4,7 @@ import { blog_data } from '../assets/assets';
 
 const Blog = () => {
 
-const {id}=useParams();
-const [data,setData]=useState(null);
 
-const fetchBlogData=async()=>{
-    blog_data
-}
 
   return (
     <div>
